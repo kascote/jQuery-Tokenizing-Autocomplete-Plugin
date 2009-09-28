561fb7fae0169cb252ae6d5bdddbb7dee87e1cc4
Felix Clack <felixclack@gmail.com>
2009-09-28 17:34:08 +0100

    Revert

--- src/jquery.tokeninput.js
+++ src/jquery.tokeninput.js
@@ -9,6 +9,7 @@
  */
 
 (function($) {
+
 $.fn.tokenInput = function (url, options) {
     var settings = $.extend({
         url: url,
@@ -25,9 +26,7 @@ $.fn.tokenInput = function (url, options) {
         contentType: "json",
         queryParam: "q",
         onResult: null,
-        canCreate: false,
-        editEnabled: false,
-        linkedTagPrefix: null
+        canCreate: false
     }, options);
 
     settings.classes = $.extend({
@@ -36,7 +35,6 @@ $.fn.tokenInput = function (url, options) {
         tokenDelete: "token-input-delete-token",
         selectedToken: "token-input-selected-token",
         highlightedToken: "token-input-highlighted-token",
-        linkedToken: "token-input-linked-token",
         dropdown: "token-input-dropdown",
         dropdownItem: "token-input-dropdown-item",
         dropdownItem2: "token-input-dropdown-item2",
@@ -46,7 +44,6 @@ $.fn.tokenInput = function (url, options) {
 
     return this.each(function () {
         var list = new $.TokenList(this, settings);
-        this.get_tokenInput = function() { return list };
     });
 };
 
@@ -87,17 +84,12 @@ $.TokenList = function (input, settings) {
     // Keep track of the timeout
     var timeout;
 
-    // Keep track of whether editing is enabled or not
-    var editEnabled = settings.editEnabled;
-
     // Create a new text input an attach keyup events
     var input_box = $("<input type=\"text\">")
         .css({
             outline: "none",
         })
         .focus(function () {
-            if (!editEnabled) { return false; }
-
             if (settings.tokenLimit == null || settings.tokenLimit != token_count) {
                 show_dropdown_hint();
             }
@@ -212,29 +204,19 @@ $.TokenList = function (input, settings) {
 
     // The list to store the token items in
     var token_list = $("<ul />")
-    	  .css('width', hidden_input.width())
         .addClass(settings.classes.tokenList)
         .insertAfter(hidden_input)
         .click(function (event) {
-            if(editEnabled) {
-                var li = get_element_from_event(event, "li");
-                if(li && li.get(0) != input_token.get(0)) {
-                    toggle_select_token(li);
-                    return false;
-                } else {
-                    input_box.focus();
+            var li = get_element_from_event(event, "li");
+            if(li && li.get(0) != input_token.get(0)) {
+                toggle_select_token(li);
+                return false;
+            } else {
+                input_box.focus();
 
-                    if(selected_token) {
-                        deselect_token($(selected_token), POSITION.END);
-                    }
+                if(selected_token) {
+                    deselect_token($(selected_token), POSITION.END);
                 }
-            }
-            else {
-                var li = get_element_from_event(event, "li");
-                if (!li || !settings.linkedTagPrefix ) { return; }
-
-                window.location.href = settings.linkedTagPrefix
-                    + $.data(li.get(0), "tokeninput").name;
             }
         })
         .mouseover(function (event) {
@@ -242,17 +224,11 @@ $.TokenList = function (input, settings) {
             if(li && selected_token !== this) {
                 li.addClass(settings.classes.highlightedToken);
             }
-            if(li) {
-                li.addClass(settings.classes.linkedToken);
-            }
         })
         .mouseout(function (event) {
             var li = get_element_from_event(event, "li");
             if(li && selected_token !== this) {
                 li.removeClass(settings.classes.highlightedToken);
-            }
-            if(li) {
-                li.removeClass(settings.classes.linkedToken);
             }
         })
         .mousedown(function (event) {
@@ -285,10 +261,35 @@ $.TokenList = function (input, settings) {
 
     // Pre-populate list if items exist
     function init_list () {
-        if(settings.prePopulate) {
-            $.each(settings.prePopulate, function(i, item) {
-                insert_token(item.id, item.name);
-            });
+        li_data = settings.prePopulate;
+        if(li_data && li_data.length) {
+            for(var i in li_data) {
+                var this_token = $("<li><p>"+li_data[i].name+"</p> </li>")
+                    .addClass(settings.classes.token)
+                    .insertBefore(input_token);
+
+                $("<span>x</span>")
+                    .addClass(settings.classes.tokenDelete)
+                    .appendTo(this_token)
+                    .click(function () {
+                        delete_token($(this).parent());
+                        return false;
+                    });
+
+                $.data(this_token.get(0), "tokeninput", {"id": li_data[i].id, "name": li_data[i].name});
+
+                // Clear input box and make sure it keeps focus
+                input_box
+                    .val("")
+                    .focus();
+
+                // Don't show the help dropdown, they've got the idea
+                hide_dropdown();
+
+                // Save this token id
+                var id_string = li_data[i].id + ","
+                hidden_input.val(hidden_input.val() + id_string);
+            }
         }
     }
 
@@ -447,22 +448,6 @@ $.TokenList = function (input, settings) {
         selected_dropdown_item = null;
     }
 
-    function hide_inputbox () {
-        input_box.hide().empty();
-    }
-
-    function hide_tagdelete () {
-        $('.' + settings.classes.tokenDelete, token_list).hide();
-    }
-
-    function show_tagdelete () {
-        $('.' + settings.classes.tokenDelete, token_list).show();
-    }
-
-    function show_inputbox () {
-        input_box.show();
-    }
-
     function show_dropdown_searching () {
     	if(settings.searchingText.length > 0) {
 	        dropdown
@@ -592,23 +577,10 @@ $.TokenList = function (input, settings) {
             };
 
             if(settings.method == "POST") {
-                $.post(settings.url, settings.queryParam + "=" + query, callback, settings.contentType);
-            } else {
-                $.get(settings.url + queryStringDelimiter + settings.queryParam + "=" + query, {}, callback, settings.contentType);
-            }
-        }
-    }
-
-    this.editable = function (enable) {
-        editEnabled = enable === undefined ? true : enable;
-        if (editEnabled) {
-            show_inputbox();
-            show_tagdelete();
-        }
-        else {
-            hide_dropdown();
-            hide_inputbox();
-            hide_tagdelete();
+			    $.post(settings.url + queryStringDelimiter + settings.queryParam + "=" + query, {}, callback, settings.contentType);
+		    } else {
+		        $.get(settings.url + queryStringDelimiter + settings.queryParam + "=" + query, {}, callback, settings.contentType);
+		    }
         }
     }
 };
